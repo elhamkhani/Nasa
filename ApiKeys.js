68d408fa4240d56a42073ee8ecf8d03ad12cc8c5
G\Elham.Khani <elham.khani@geeks.ltd.uk>
2017-11-27 11:49:06 +0000

    Changing the folder's path

---
+++ ApiKeys.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Elham.Khani on 12/01/2016.
+ */
+
+var config = {
+     
+googleKey : 'AIzaSyCcAd_LTLqtzoCqTHShUxNjGLPxfCsTABo',
+     
+nasaKey : 'Erj1DXpDoYLc8yl2bNOILUbprBGKQBLhKSo7BlRn',
+
+};
